650714b6fb4b11d0eb3e5defa697666455bdd9c0
Alan Vincent <alan.d.vincent@gmail.com>
2018-02-21 21:57:04 -0700

    created base route to keep user on same page

---
+++ src/Sports.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Sports extends Component {
+  render() {
+    return (
+    <div>
+    sports stuff
+    </div>
+    );
+  }
+}
+
+export default Sports;
