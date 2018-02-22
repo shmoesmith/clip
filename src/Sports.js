a5e7f8774afcd970a390fadd7759bab55e5caafe
Alan Vincent <alan.d.vincent@gmail.com>
2018-02-21 22:38:23 -0700

    added title, button, and basic page format

--- src/Sports.js
+++ src/Sports.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import { Header, Button, Segment } from 'semantic-ui-react'
 import './App.css';
 
 class Sports extends Component {
+
+  state = {}
+  componentDidMount() {
+    this.setList();
+  }
+
+  setList = () => {
+    let sports = [
+      { id: 'ab123aa', name: 'Baseball' },
+      { id: 'ab124aa', name: 'Golf' },
+      { id: 'ab125aa', name: 'Hockey' },
+      { id: 'ab126bb', name: 'Soccer' },
+      { id: 'ab127bb', name: 'Football' },
+      { id: 'ab128cc', name: 'Skiing' },
+      { id: 'ab129dd', name: 'Ping Pong' },
+      { id: 'ab130cc', name: 'Foosball' },
+    ];
+    this.setState({ sportList: sports})
+  }
+
+  handleclick = (e) => {
+    e.preventDevault();
+  }
+
   render() {
     return (
     <div>
-    sports stuff
+    <Header as='h3'>
+    Click on your least favorite sports below to delete them until you have three left
+    </Header>
+    <Segment>
+
+    </Segment>
+    <Button primary>Reset List!</Button>
     </div>
     );
   }
