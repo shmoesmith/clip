6f508836488686f2ea66819f0a0b993f2dbaafe4
Alan Vincent <alan.d.vincent@gmail.com>
2018-02-22 18:15:27 -0700

    changed logic for alerting to no more sports

--- src/Sports.js
+++ src/Sports.js
@@ -3,6 +3,7 @@ import { Header, Button, Image } from 'semantic-ui-react'
 import './App.css';
 import trash from './trash-10-48.jpg'
 
+
 class Sports extends Component {
 
   state = {}
@@ -10,12 +11,7 @@ class Sports extends Component {
   componentWillMount() {
     this.setList();
   }
-  componentDidUpdate() {
-    if (this.state.sportList.length === 0) {
-      alert("Oh no, you deleted all the sports :(")
-    }
-  }
-
+  
   setList = () => {
     let sports = [
       { id: 'ab123aa', name: 'Baseball' },
@@ -36,17 +32,22 @@ class Sports extends Component {
     let newList = currentList.filter(sport => sport.name != e.target.name)
     let deletedSport = document.getElementById(e.target.value)
     deletedSport.className = "animated"
-
-
-    setTimeout( () => { this.setState({ sportList: newList }) },3000)
+    setTimeout( () => { this.setState({ sportList: newList }) },1000)
   }
 
   render() {
+    let heading;
+    if (this.state.sportList.length == 0) {
+      heading = "Oh no, you deleted all the sports :("
+    } else {
+      heading = "Click on your least favorite sports below to delete them until you have three left"
+    }
     return (
     <div>
-      <Header as='h3'>
-      Click on your least favorite sports below to delete them until you have three left
+      <Header className="instructions" as='h3'>
+    {heading}
       </Header>
+
       <div className="listStage">
         <ul>
 
